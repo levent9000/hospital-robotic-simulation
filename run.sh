d71104918e9989ce9654add271b41c0213d809f3
levent9000 <levent9000@pm.me>
2026-04-22 17:01:43 +0200

    Update run script to use correct renderer and model paths

--- run.sh
+++ run.sh
@@ -3,6 +3,5 @@ set -e
 
 cd "$(dirname "$0")"
 source .venv/bin/activate
-
 export GZ_SIM_RESOURCE_PATH=$PWD/models:$PWD/fuel_models:$PWD/worlds
-gz sim worlds/hospital.world
+gz sim --render-engine ogre worlds/hospital.world
