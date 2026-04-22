f8c7fea22b7be7c1c6b4ee20596a36a87b608f96
levent9000 <levent9000@pm.me>
2026-04-22 17:19:52 +0200

    Update run.sh ogre2 from ogre

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ set -e
 cd "$(dirname "$0")"
 source .venv/bin/activate
 export GZ_SIM_RESOURCE_PATH=$PWD/models:$PWD/fuel_models:$PWD/worlds
-gz sim --render-engine ogre worlds/hospital.world
+gz sim --render-engine ogre2 worlds/hospital.world
